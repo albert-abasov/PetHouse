0722bf13f9a3692ffe28f67ca4827edb74b7f1cc
Albert <abasovalik2001@gmail.com>
2022-03-04 12:38:01 +0200

    Revert "finished with REST for all entities"
    This reverts commit f95f5960ebe3894fa9b2cb028cc451418a2df862.

--- src/main/resources/db/migration/V2__create_pets_table.sql
+++ src/main/resources/db/migration/V2__create_pets_table.sql
@@ -1,10 +1,8 @@
-create type pet_type as enum ('CAT', 'DOG');
-
 create table pets
 (
     id           SERIAL primary key not null,
     name         varchar(30)        not null,
-    type         pet_type           not null,
+    type         varchar(30)           not null,
     breed        varchar(30),
     pet_owner_id int                not null,
     description  varchar(300)       not null,
